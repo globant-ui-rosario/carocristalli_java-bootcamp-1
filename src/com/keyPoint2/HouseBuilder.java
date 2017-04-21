28d2a4a93fa8911b296034e0342e80206ceea25c
Cari <carocristalli@gmail.com>
2017-04-21 14:57:13 -0300

    Key Point 2 completed.

---
+++ src/com/keyPoint2/HouseBuilder.java
@@ -0,0 +1,15 @@
+package com.keyPoint2;
+
+//Builder Interface
+
+public interface HouseBuilder {
+	
+	public void buildAddress();
+	
+	public void buildRooms();
+	
+	public void buildSquaredMeters();
+	
+	public House getHouse();
+
+}
